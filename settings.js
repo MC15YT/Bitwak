8ae454060e0b05d83fd86065ce8f180c7675392b
MC15YT <100246501+MC15YT@users.noreply.github.com>
2023-06-22 22:18:08 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
+global.creator = 'mcdevz'// yourname
+global.MONGO_DB_URI = "mongodb+srv://MC15:MC15GG@cluster0.wnm5acj.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.your_email = "mc15gg@gmail.com" //email
+global.email_password = "mcgg" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
